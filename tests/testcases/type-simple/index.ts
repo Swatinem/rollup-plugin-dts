d64d0bdf319d9036fb5f6fc57cede6df5359c05b
Carlos Precioso <kalprestito@gmail.com>
2019-07-30 16:00:35 +0200

    Add test for simple types

---
+++ tests/testcases/type-simple/index.ts
@@ -0,0 +1,17 @@
+export declare const A: "string";
+export declare const B: 8;
+export declare const C: void;
+export declare const D: unknown;
+export declare const E: any;
+export declare const F: boolean;
+export declare const G: number;
+export declare const H: string;
+export declare const I: object;
+export declare const J: null;
+export declare const K: undefined;
+export declare const L: symbol;
+export declare const M: never;
+export interface N {
+  foo(): this;
+}
+export declare const O: bigint;
