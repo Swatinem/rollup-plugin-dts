d4dd901f6f052910fbbdb12c653f8e721a0a912e
Carlos Precioso <kalprestito@gmail.com>
2019-07-30 16:00:35 +0200

    Add test for simple types

---
+++ tests/testcases/type-simple/expected.d.ts
@@ -0,0 +1,18 @@
+declare const A: "string";
+declare const B: 8;
+declare const C: void;
+declare const D: unknown;
+declare const E: any;
+declare const F: boolean;
+declare const G: number;
+declare const H: string;
+declare const I: object;
+declare const J: null;
+declare const K: undefined;
+declare const L: symbol;
+declare const M: never;
+interface N {
+    foo(): this;
+}
+declare const O: bigint;
+export { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O };
