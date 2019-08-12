b30f0424b41b3a19f60cef50765276964fe6cef4
Arpad Borsos <arpad.borsos@googlemail.com>
2019-08-12 08:59:20 +0200

    regenerate fixtures

--- tests/testcases/multiple-entries/expected.d.ts
+++ tests/testcases/multiple-entries/expected.d.ts
@@ -1,8 +1,8 @@
 // a.d.ts
-export { A } from './chunk-ecf69fe7.d.ts';
+export { A } from './common-ecf69fe7.d.ts';
 // b.d.ts
-export { B } from './chunk-ecf69fe7.d.ts';
-// chunk-ecf69fe7.d.ts
+export { B } from './common-ecf69fe7.d.ts';
+// common-ecf69fe7.d.ts
 interface A {
 }
 interface B {
--- tests/testcases/using-ts-files/expected.d.ts
+++ tests/testcases/using-ts-files/expected.d.ts
@@ -1,8 +1,8 @@
 // a.d.ts
-export { A } from './chunk-bbd98d0c.d.ts';
+export { A } from './common-bbd98d0c.d.ts';
 // b.d.ts
-export { B } from './chunk-bbd98d0c.d.ts';
-// chunk-bbd98d0c.d.ts
+export { B } from './common-bbd98d0c.d.ts';
+// common-bbd98d0c.d.ts
 interface A {
 }
 interface B {
