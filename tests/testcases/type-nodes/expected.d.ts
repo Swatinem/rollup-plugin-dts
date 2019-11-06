0c3000b941e8afe6cc15deee6bb2c732c1215874
Arpad Borsos <arpad.borsos@googlemail.com>
2019-11-06 10:21:53 +0100

    bump ts version and add test for new assertion functions

--- tests/testcases/type-nodes/expected.d.ts
+++ tests/testcases/type-nodes/expected.d.ts
@@ -28,10 +28,13 @@ interface N {
 }
 interface O {
 }
+interface P {
+}
 declare function parenthesized(a: (A)): (B);
 declare function union(a: C | D): E | F;
 declare function intersection(a: G & H): I & J;
 declare function operator(a: keyof K): void;
 declare function arrayAndTuple(a: [L, M]): N[];
 declare function predicate(a: any): a is O;
-export { arrayAndTuple, intersection, operator, parenthesized, predicate, union };
+declare function assertion(a: any): asserts a is P;
+export { arrayAndTuple, assertion, intersection, operator, parenthesized, predicate, union };
--- tests/testcases/type-nodes/index.d.ts
+++ tests/testcases/type-nodes/index.d.ts
@@ -28,10 +28,12 @@ interface N {
 }
 interface O {
 }
+interface P {
+}
 export declare function parenthesized(a: (A)): (B);
 export declare function union(a: C | D): E | F;
 export declare function intersection(a: G & H): I & J;
 export declare function operator(a: keyof K): void;
 export declare function arrayAndTuple(a: [L, M]): N[];
 export declare function predicate(a: any): a is O;
-export {};
+export declare function assertion(a: any): asserts a is P;
