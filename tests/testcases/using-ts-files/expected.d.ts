f2e0e3db0f4e3d6e99cb0514d89e66fa47a500e1
Arpad Borsos <arpad.borsos@googlemail.com>
2020-05-17 11:08:18 +0200

    Remove file extensions from imports/re-exports in chunks
    Otherwise TS would complain

--- tests/testcases/using-ts-files/expected.d.ts
+++ tests/testcases/using-ts-files/expected.d.ts
@@ -1,7 +1,7 @@
 // a.d.ts
-export { A } from './common-90654e94.d.ts';
+export { A } from './common-90654e94';
 // b.d.ts
-export { B } from './common-90654e94.d.ts';
+export { B } from './common-90654e94';
 // common-90654e94.d.ts
 interface A {
 }
