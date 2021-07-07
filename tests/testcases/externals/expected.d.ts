6148b0d9b1e7f46e81d4579b3bf50e5d62f807b6
Arpad Borsos <swatinem@swatinem.de>
2021-07-07 11:53:35 +0200

    update deps

--- tests/testcases/externals/expected.d.ts
+++ tests/testcases/externals/expected.d.ts
@@ -1,5 +1,5 @@
 export { ReactFragment } from 'react';
-// Type definitions for D3JS d3-drag module 2.0
+// Type definitions for D3JS d3-drag module 3.0
 /**
  * Container element type usable for mouse/touch functions
  */
