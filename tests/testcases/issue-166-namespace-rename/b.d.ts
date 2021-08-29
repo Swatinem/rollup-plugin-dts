1c3c7916f06ceddc6403c6b4d43ea585334472a0
Arpad Borsos <swatinem@swatinem.de>
2021-08-29 10:42:05 +0200

    add failing test for issue 166

---
+++ tests/testcases/issue-166-namespace-rename/b.d.ts
@@ -0,0 +1,5 @@
+declare const Item: () => void;
+declare namespace B {
+  export { Item };
+}
+export { B };
---
+++ tests/testcases/issue-166-namespace-rename/expected.d.ts
@@ -0,0 +1,9 @@
+declare const Item$1: () => void;
+declare namespace A {
+  export { Item$1 };
+}
+declare const Item: () => void;
+declare namespace B {
+  export { Item };
+}
+export { A, B };
