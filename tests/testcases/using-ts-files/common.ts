a985a6fe01cdf5debcb2bd9d421a6f9bfe78b609
hyrious <hyrious@outlook.com>
2024-10-29 01:34:10 +0800

    fix: force emit types even when there's errors (#320)

--- tests/testcases/using-ts-files/common.ts
+++ tests/testcases/using-ts-files/common.ts
@@ -1,3 +1,6 @@
 export interface A {}
 export interface B {}
 export interface unused {}
+// The following code triggers `Unused '@ts-expect-error' directive` error,
+// but it doesn't actually prevent the DTS from being emitted.
+// @ts-expect-error
