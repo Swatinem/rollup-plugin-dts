a2251cb97537af81aefe50a28ba23c295af96f04
kricsleo <kricsleo@163.com>
2025-02-20 11:53:04 +0800

    fix: bump the minimal version for json modules

--- tests/testcases/import-json/meta.js
+++ tests/testcases/import-json/meta.js
@@ -4,6 +4,13 @@ import path from "path";
 
 /** @type {import('../../testcases').Meta} */
 export default {
+  /**
+   * TypeScript <5.1 will generate `declare const name: ...` for JSON modules,
+   * instead of current `decalre let name: ...`.
+   * But it doesn't matter, both can work.
+   * I'll just omit the test results for TypeScript <5.1.
+   */
+  tsVersion: "5.1",
   options: {
     tsconfig: path.resolve(url.fileURLToPath(new URL(".", import.meta.url)), "tsconfig.json"),
   },
