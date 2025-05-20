70e34e629d3a72cbea6ab1dc7028a0138e91d9ce
Hyddan <daniel.hedenius@gmail.com>
2025-05-20 13:17:58 +0200

    chore: add test case for includeExternal option

---
+++ tests/testcases/include-external/expected.d.ts
@@ -0,0 +1,7 @@
+export { ReactElement } from 'react';
+// Last module patch version validated against: 3.0.0
+/**
+ * Container element type usable for mouse/touch functions
+ */
+type DragContainerElement = HTMLElement | SVGSVGElement | SVGGElement;
+export type { DragContainerElement };
