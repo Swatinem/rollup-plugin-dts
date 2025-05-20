3b4ddda5f0b27d68c74c665810894ce2fa50cd91
Hyddan <daniel.hedenius@gmail.com>
2025-05-20 12:03:58 +0200

    feat: add includeExternal option

--- src/options.ts
+++ src/options.ts
@@ -2,13 +2,17 @@ import type ts from "typescript";
 
 export interface Options {
   /**
-   * The plugin will by default flag *all* external libraries as `external`,
-   * and thus prevent them from be bundled.
+   * The plugin will by default flag *all* external libraries as `external`
+   * (unless specified through includeExternal), and thus prevent them from be bundled.
    * If you set the `respectExternal` option to `true`, the plugin will not do
    * any default classification, but rather use the `external` option as
    * configured via rollup.
    */
   respectExternal?: boolean;
+  /**
+   * A list of external modules to include types from.
+   */
+  includeExternal?: Array<string>;
   /**
    * In case you want to use TypeScript path-mapping feature, using the
    * `baseUrl` and `paths` properties, you can pass in `compilerOptions`.
@@ -25,6 +29,7 @@ export function resolveDefaultOptions(options: Options) {
     ...options,
     compilerOptions: options.compilerOptions ?? {},
     respectExternal: options.respectExternal ?? false,
+    includeExternal: options.includeExternal ?? [],
   };
 }
 
